4f96f5b953ffa8d6abd3e33cb0ed3c0d204c9b42
adriennekresha <akresha@wisc.edu>
2024-09-25 17:49:13 -0500

    milestone 1 final changes

--- app/src/main/java/com/cs407/lab3_milestone1/MainActivity.kt
+++ app/src/main/java/com/cs407/lab3_milestone1/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.cs407.lab3_milestone1
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,6 +20,42 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        }
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        submitButton.setOnClickListener {
+            showDialog()
         }
     }
+    private fun showDialog(){
+        AlertDialog.Builder(this)
+            .setTitle("Dialog Title")
+            .setMessage("This is the dialog message")
+            .setPositiveButton("Ok"){ dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.example_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item1 -> {
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.item2 -> {
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
 }
